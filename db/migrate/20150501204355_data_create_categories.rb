29c3b6b7399402e31019c24ceadd46b0033e6540
Ivan Zotov <ivanzotov@gmail.com>
2016-02-08 16:19:09 +0300

    Turn off paper_trail in migrations

--- db/migrate/20150501204355_data_create_categories.rb
+++ db/migrate/20150501204355_data_create_categories.rb
@@ -1,8 +1,10 @@
 class DataCreateCategories < ActiveRecord::Migration
   def change
+    PaperTrail.enabled = false
     Category.create!(name: 'Теннис', parent: Category.where(name: 'Стадион').first)
     Category.create!(name: 'Групповые занятия')
     Category.create!(name: 'Йога')
     Category.create!(name: 'Фитнесс')
+    PaperTrail.enabled = true
   end
 end
