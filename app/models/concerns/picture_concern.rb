097ee90f90bc79dd5e9d160a5f6ed6722ed6ea3b
serge1peshcoff <peshkov@railsmob.com>
2016-02-08 18:08:01 +0300

    Picture model pretty displaying in rails_admin

---
+++ app/models/concerns/picture_concern.rb
@@ -0,0 +1,23 @@
+module PictureConcern
+  extend ActiveSupport::Concern
+
+  included do
+    rails_admin do
+      list do
+        field :name
+        field :imageable
+        field :description
+        field :created_at
+        field :updated_at
+      end
+
+      edit do
+        field :name
+        field :imageable
+        field :description
+        field :created_at
+        field :updated_at
+      end
+    end
+  end
+end
