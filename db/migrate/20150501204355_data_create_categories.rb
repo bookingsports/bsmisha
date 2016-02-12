8146dd6cdc88ba16f9e2a45f4bc1589a6444f74c
Ivan Zotov <ivanzotov@gmail.com>
2016-02-12 19:09:39 +0300

    Fix migrations for data create categories, and fix seeds

--- db/migrate/20150501204355_data_create_categories.rb
+++ db/migrate/20150501204355_data_create_categories.rb
@@ -1,7 +1,7 @@
 class DataCreateCategories < ActiveRecord::Migration
   def change
     PaperTrail.enabled = false
-    Category.create!(name: 'Теннис', parent: Category.where(name: 'Стадион').first)
+    Category.create!(name: 'Теннис')
     Category.create!(name: 'Групповые занятия')
     Category.create!(name: 'Йога')
     Category.create!(name: 'Фитнесс')
