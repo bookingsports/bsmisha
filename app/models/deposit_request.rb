f000614c733c9c6ebc01cead60435e5350e1f433
serge1peshcoff <serge1peshcoff@mail.ru>
2016-02-12 19:42:23 +0300

    Rollback DepositRequest status due to failing build

--- app/models/deposit_request.rb
+++ app/models/deposit_request.rb
@@ -21,7 +21,7 @@ class DepositRequest < ActiveRecord::Base
   composed_of :data, class_name: "DepositRequestData", mapping: [%w(id order_id), %w(amount amount)]
   has_many :deposit_responses
 
-  enumerize :status, in: [:pending, :success, :failure]
+  enum status: [:pending, :success, :failure]
 
   after_initialize :set_pending, if: :new_record?
 
