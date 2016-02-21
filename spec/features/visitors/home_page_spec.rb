e6e51a55444b9a7a2e092a145999e33e4f211eb4
Ivan Zotov <ivanzotov@gmail.com>
2016-02-21 14:15:49 +0300

    Add first selenium test

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-# Feature: Home page
-#   As a visitor
-#   I want to visit a home page
-#   So I can learn more about the website
-RSpec.feature "Home page" do
-  # Scenario: Visit the home page
-  #   Given I am a visitor
-  #   When I visit the home page
-  #   Then I see "Welcome"
-  scenario "visit the home page" do
+RSpec.feature 'Home page' do
+  before(:each) do
     visit root_path
-    expect(page).to have_content("Спортивные объекты для бронирования")
+  end
+
+  scenario 'visit the home page' do
+    expect(page.status_code).to be(200)
+  end
+
+  scenario 'home page title check', js: true do
+    visit root_path
+    expect(page.title).to eq('Booking Sports - Бронируйте спорт надежно')
   end
 end
