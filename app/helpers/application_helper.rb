dd93590a00a4cfd60e914259e0b1eff8fc7dd64d
Ivan Zotov <ivanzotov@gmail.com>
2016-02-25 10:05:41 +0300

    Add mem cache and try main cache key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
       dashboard_employments_path
     end
   end
+
+  def cache_key_for_categories
+    count          = Category.count
+    max_updated_at = Category.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "categories/all-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_stadiums
+    count          = Stadium.count
+    max_updated_at = Stadium.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "stadiums/all-#{count}-#{max_updated_at}"
+  end
 end
