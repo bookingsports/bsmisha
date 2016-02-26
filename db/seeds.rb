3255ad1736bbba60a0599630b6053f1604abfe5c
serge1peshcoff <peshkov@railsmob.com>
2016-02-26 15:48:25 +0300

    Rewritten seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 def create_user(model, email, password)
   user = model.find_or_create_by!(email: email) do |user|
+    user.name = "Test User"
     user.password = password
     user.password_confirmation = password
   end
@@ -16,12 +17,30 @@ def create_user(model, email, password)
   user
 end
 
-create_user(Admin, 'admin@bookingsports.ru', 'changeme')
-stadium_user = create_user(StadiumUser, 'stadium@bookingsports.ru', 'changeme')
-create_user(CoachUser, 'coach@bookingsports.ru', 'changeme')
-create_user(Customer, 'customer@bookingsports.ru', 'changeme')
+categories = ['Теннис', 'Групповые занятия', 'Йога', 'Фитнесс']
+categories.each { |name| Category.create! name: name }
 
-c = Category.create!(name: "Футбол", parent: Category.create(name: "Стадион"))
-stadium_user.stadium = Stadium.create!(name: 'Арена "Открытие"', category: c, phone: '123')
-stadium_user.stadium.courts.create! name: 'Первый'
-stadium_user.stadium.courts.create! name: 'Второй'
+create_user(Admin, 'admin@bookingsports.ru', 'changeme')
+coach_user = create_user(CoachUser,  'coach@bookingsports.ru', 'changeme')
+customer = create_user(Customer, 'customer@bookingsports.ru', 'changeme')
+
+stadium_names = ["Opentennis 2015", "Group Stadium", "Yoga Stadium", "Fitness Stadium"]
+stadium_addresses = ["ул. Большая Филевская, 20, Москва, Россия, 121309", "Перовская ул., 20, Москва, Россия, 111398", "ул. Пришвина, 3, Москва, Россия, 127549", "Котляковская ул., 1с17, Москва, Россия, 115201"]
+
+(0..3).each do |number|
+  stadium_user = create_user(StadiumUser, "stadium#{number}@bookingsports.ru", 'changeme')
+  stadium_user.stadium.update(name: stadium_names[number], category: Category.where(name: categories[number]).first, phone: '+7985444484', address: stadium_addresses[number], status: :active, opens_at: "07:00", closes_at: "23:00")
+  stadium_user.stadium.courts.create! name: 'Первый', price: 500
+  stadium_user.stadium.courts.create! name: 'Второй', price: 1000
+  stadium_user.wallet.deposits.create amount: 100000, status: :active
+
+  service = Service.create name: "Инструктор"
+  product_service = stadium_user.stadium.product_services.create price: 500, service: service
+
+  stadium_user.account.update(number: "30101810200000000700", company: "АО “Райффайзенбанк”, 129090, Россия, г. Москва, ул. Троицкая, д.17/1", inn: "7744000302", kpp: "775001001", bik: "044525700", agreement_number: "1234567890", date: Time.now)
+end
+
+coach_user.account.update(number: "30101810200000000700", company: "АО “Райффайзенбанк”, 129090, Россия, г. Москва, ул. Троицкая, д.17/1", inn: "7744000302", kpp: "775001001", bik: "044525700", agreement_number: "1234567890", date: Time.now)
+
+customer.wallet.deposits.create amount: 100000, status: :active
+coach_user.wallet.deposits.create amount: 100000, status: :active
