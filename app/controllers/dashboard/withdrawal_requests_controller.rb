ea2a948e7057a646850f58b66bf87cd501afe510
serge1peshcoff <peshkov@railsmob.com>
2016-04-05 10:46:44 +0300

    Fixed StadiumUser creating WithdrawingRequest

--- app/controllers/dashboard/withdrawal_requests_controller.rb
+++ app/controllers/dashboard/withdrawal_requests_controller.rb
@@ -5,18 +5,25 @@ class Dashboard::WithdrawalRequestsController < DashboardController
   end
 
   def create
-    if current_user.type == 'CoachUser' && (current_user.coach.blank? || current_user.coach.account.blank?)
-      redirect_to edit_account_dashboard_product_url, alert: "Укажите реквизиты для вывода!"
-    else
-      a = current_user.coach.account
-      if a.number.blank? || a.company.blank? || a.inn.blank? || a.kpp.blank? || a.bik.blank? || a.agreement_number.blank? || a.date.blank?
-        redirect_to edit_account_dashboard_product_url, alert: "Не все реквизиты заполнены!"
+    if current_user.type == 'CoachUser'
+      if current_user.coach.blank? || current_user.coach.account.blank?
+        redirect_to edit_account_dashboard_product_url, alert: "Укажите реквизиты для вывода!"
       else
-        @request = current_user.wallet.withdrawal_requests.new request_params
-        @request.save
+        a = current_user.coach.account
+        if a.number.blank? || a.company.blank? || a.inn.blank? || a.kpp.blank? || a.bik.blank? || a.agreement_number.blank? || a.date.blank?
+          redirect_to edit_account_dashboard_product_url, alert: "Не все реквизиты заполнены!"
+        else
+          @request = current_user.wallet.withdrawal_requests.new request_params
+          @request.save
 
-        redirect_to dashboard_withdrawal_requests_url
+          redirect_to dashboard_withdrawal_requests_url
+        end
       end
+    else
+      @request = current_user.wallet.withdrawal_requests.new request_params
+      @request.save
+
+      redirect_to dashboard_withdrawal_requests_url
     end
   end
 
