19ad5533d4cb6ecf72fb95c5be9e39e478850637
serge1peshcoff <peshkov@railsmob.com>
2016-04-11 11:40:07 +0300

    Added Confirmed field to events

--- db/migrate/20160411072134_add_discounts_to_daily_price_rules.rb
+++ db/migrate/20160411072134_add_discounts_to_daily_price_rules.rb
@@ -1,7 +1,5 @@
 class AddDiscountsToDailyPriceRules < ActiveRecord::Migration
   def change
     add_column :daily_price_rules, :is_discount, :boolean, default: false
-
-    DailyPriceRule.update_all is_discount: false
   end
 end
