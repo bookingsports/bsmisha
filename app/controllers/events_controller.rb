7bd0ebfee7b6f453955a199daacc9ac28ff88946
serge1peshcoff <serge1peshcoff@mail.ru>
2016-06-09 18:41:54 +0300

    Fixed crashing when no params are presented on one_day page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -57,8 +57,8 @@ class EventsController < ApplicationController
   end
 
   def one_day
-    @stadium = params[:schedule_form][:stadium].present? ? Stadium.find(params[:schedule_form][:stadium]) : Stadium.active.first
-    @day = params[:schedule_form]["day(1i)"].present? ? DateTime.new(params[:schedule_form]["day(1i)"].to_i, params[:schedule_form]["day(2i)"].to_i, params[:schedule_form]["day(3i)"].to_i) : DateTime.now
+    @stadium = params[:schedule_form].present? ? Stadium.find(params[:schedule_form][:stadium]) : Stadium.active.first
+    @day = params[:schedule_form].present? ? DateTime.new(params[:schedule_form]["day(1i)"].to_i, params[:schedule_form]["day(2i)"].to_i, params[:schedule_form]["day(3i)"].to_i) : DateTime.now
   end
 
   def private
