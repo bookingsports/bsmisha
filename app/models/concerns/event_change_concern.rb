6a00f792f997ab72f2183ea725450ef3f5f2859c
serge1peshcoff <serge1peshcoff@mail.ru>
2016-06-15 23:09:21 +0300

    Fixed crashing when creating/editing/viewing EventChange

--- app/models/concerns/event_change_concern.rb
+++ app/models/concerns/event_change_concern.rb
@@ -7,18 +7,24 @@ module EventChangeConcern
       visible false
       list do
         field :event
-        field :status
-        field :summary
         field :order
+        field :old_start
+        field :old_stop
+        field :new_start
+        field :new_stop
+        field :old_price
         field :created_at
         field :updated_at
       end
 
       edit do
         field :event
-        field :status
-        field :summary
         field :order
+        field :old_start
+        field :old_stop
+        field :new_start
+        field :new_stop
+        field :old_price
       end
     end
   end
