d4bc52109da85c0c25a23880c2680e6701ee820b
serge1peshcoff <serge1peshcoff@mail.ru>
2016-07-07 13:06:17 +0300

    Fixed crashing main page when a stadium has no category

--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -9,7 +9,7 @@ module VisitorsHelper
 
   def stadium_get_category_icon(stadium)
     if stadium.active? || stadium.pending?
-      stadium.category.icon.present? ? stadium.category.icon.thumb.url : "gray-icon.png"
+      (stadium.category.present? && stadium.category.icon.present?) ? stadium.category.icon.thumb.url : "gray-icon.png"
     elsif stadium.locked?
       "gray-icon.png"
     end
