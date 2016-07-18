52ff5eb850a534d9c8c8a9b91330eb6bcd89cb54
serge1peshcoff <serge1peshcoff@mail.ru>
2016-07-18 11:25:34 +0300

    Displaying working hours properly for grid scheduler

--- app/assets/javascripts/grid_schedule_view.js
+++ app/assets/javascripts/grid_schedule_view.js
@@ -7,12 +7,17 @@
 url = window.location.pathname + '/events';
 kendo.culture('ru-RU');
 
+opens_at = new Date(gon.opens_at);
+closes_at = new Date(gon.closes_at);
+
 $("#scheduler").kendoScheduler({
   date: new Date(),
   allDaySlot: false,
   startTime: new Date('2013/6/13 07:00 AM'),
   height: 700,
   mobile: true,
+  workDayStart: opens_at,
+  workDayEnd: closes_at,
   views: [
     'day',
     {type: 'week', selected: true},
