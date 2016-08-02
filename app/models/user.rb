1cbba19cc0273f3796489dfc8a6dc2f182604096
serge1peshcoff <serge1peshcoff@mail.ru>
2016-08-02 14:07:42 +0300

    Queries optimization when loading info about events

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,11 +52,11 @@ class User < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
 
   def total(options = {})
-    events_maybe_scoped_by(options).unpaid.unconfirmed.future.map(&:price).inject(:+) || 0
+    events_maybe_scoped_by(options).unconfirmed.future.map(&:price).inject(:+) || 0
   end
 
   def total_hours(options = {})
-    events_maybe_scoped_by(options).unpaid.unconfirmed.future.map{|e| e.duration_in_hours * e.occurrences}.inject(:+) || 0
+    events_maybe_scoped_by(options).unconfirmed.future.map{|e| e.duration_in_hours * e.occurrences}.inject(:+) || 0
   end
 
   def total_recoupments(area = {})
