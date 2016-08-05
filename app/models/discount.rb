0ad9704bc6909d58176d1a31d1adfcb1cc8186c5
serge1peshcoff <serge1peshcoff@mail.ru>
2016-08-05 11:07:49 +0300

    Validating discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,8 @@ class Discount < ActiveRecord::Base
 
   belongs_to :user, required: true
   belongs_to :area, required: true
+
+  validates :user, :value, :area, presence: true
+  validates :area_id, uniqueness: { scope: :user_id }
+  validates :value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
