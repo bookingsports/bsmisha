c9f1ccabc26f96d40346a58acc7cb2cc231e7884
ViktoriyaDanilova <realy_love@mail.ru>
2018-03-31 00:03:03 +0300

    fix application

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,8 @@ module TennisRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
+    config.assets.initialize_on_precompile = false
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
